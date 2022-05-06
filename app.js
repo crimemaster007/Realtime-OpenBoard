ed814d4203ef0dd4db8a9353f5269b7b556859da
crimemaster007 <prachand9211@gmail.com>
2022-05-06 18:11:13 +0530

    my project

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express"); // access
+const socket = require("socket.io");
+
+const app = express(); //Initiliazed and server ready
+
+app.use(express.static("public"));
+
+let port = 5000;
+let server = app.listen(port, () => {
+    console.log("Listening to port " + port);
+})
+
+let io = socket(server);
+io.on("connection", (socket) => {
+    console.log("Made socket connection");
+    //Recieved data
+    socket.on("beginPath", (data) => {
+        //data from frontend
+        //Now transfer data to all connected computers
+        io.sockets.emit("beginPath", data);
+    })
+
+    socket.on("drawStroke", (data) => {
+        //data from frontend
+        //Now transfer to all connected computers
+        io.sockets.emit("drawStroke", data);
+
+    })
+
+    socket.on("undoRedoCanvas", (data) => {
+        //data from frontend
+        // Now transfer data to all connected computers
+        io.sockets.emit("undoRedoCanvas", data);
+    })
+})
